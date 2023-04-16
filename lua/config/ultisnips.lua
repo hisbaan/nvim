60a2092c513e9afa2eb84fa5085f3c2c26baa46e
hisbaan <hisbaan@gmail.com>
2023-04-15 23:42:28 -0400

    init: add config

---
+++ lua/config/ultisnips.lua
@@ -0,0 +1,7 @@
+-- Key bindings
+vim.g.UltiSnipsExpandTrigger = "<Tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<S-Tab>"
+
+-- Split when editing
+vim.g.UltiSnipsEditSplit = "vertical"
