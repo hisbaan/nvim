fc448873453082c1a877f9d1976a1f7ac42f750b
Hisbaan Noorani <hisbaan.noorani@freshcityfarms.com>
2023-05-26 13:27:50 -0400

    fix: fix plugins without explicit config

--- lua/config/neogen.lua
+++ lua/config/neogen.lua
@@ -1,4 +1,5 @@
 return {
     'danymat/neogen',
     dependencies = 'nvim-treesitter/nvim-treesitter',
+    config = true
 }
--- lua/config/nvim-lastplace.lua
+++ lua/config/nvim-lastplace.lua
@@ -1,3 +1,4 @@
 return {
     'ethanholz/nvim-lastplace',
+    config = true
 }
