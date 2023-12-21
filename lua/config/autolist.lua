1ecac4d2380674d941c6ee444e24f679c673968d
hisbaan <hisbaan@gmail.com>
2023-12-20 21:08:12 -0500

    fix: remove conflicting bindings

--- lua/config/autolist.lua
+++ lua/config/autolist.lua
@@ -52,14 +52,14 @@ return {
             },
         })
 
-        vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-        vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+        -- vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+        -- vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
         -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
         vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
         vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
         vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
         vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-        vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+        -- vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
         -- cycle list types with dot-repeat
         vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
