0493adb86be7747fea9d0d4f0eb9438f45c10491
hisbaan <hisbaan@gmail.com>
2024-01-23 09:27:58 -0500

    feat(obsidian): update obsidian plugin configuration

---
+++ lua/config/dataview.lua
@@ -0,0 +1,11 @@
+return {
+    dir = "/home/hisbaan/projects/dataview.nvim",
+    event = {
+        "BufEnter " .. vim.fn.expand "~" .. "/Documents/obsidian**",
+    },
+    -- ft = {"markdown"},
+    opts = {
+        vault_dir = "~/Documents/obsidian/",
+        buffer_type = "float",
+    }
+}
