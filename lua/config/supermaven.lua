e8b5aa98b82e613ecb93071f91a0a394e358642e
hisbaan <hisbaan@gmail.com>
2025-05-09 17:43:42 -0400

    feat: add supertab, supermaven, and which-key

---
+++ lua/config/supermaven.lua
@@ -0,0 +1,25 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  config = true,
+  lazy = false,
+  opts = function()
+    local C = require("catppuccin.palettes").get_palette()
+
+    return {
+      keymaps = {
+        accept_suggestion = "<C-l>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+      ignore_filetypes = {},
+      color = {
+        suggestion_color = C.overlay0,
+      },
+      log_level = "info",
+      disable_keymaps = false,
+      condition = function()
+        return true
+      end,
+    }
+  end
+}
