132580bc81685150b5162056684cdcfba3588c22
hisbaan <hisbaan@gmail.com>
2025-10-01 23:05:41 -0400

    feat: add formatters to flags.lua

--- lua/config/null-ls.lua
+++ lua/config/null-ls.lua
@@ -2,12 +2,15 @@ return {
     'nvimtools/none-ls.nvim',
     config = function()
         local null_ls = require("null-ls")
+        local flags = require('flags')
+
+        local sources = {}
+        if flags.lsp.formatters.yamlfmt then vim.list_extend(sources, null_ls.builtins.formatting.yamlfmt) end
+        if flags.lsp.formatters.prettier then vim.list_extend(sources, null_ls.builtins.formatting.prettier) end
+        if flags.lsp.formatters.sql_formatter then vim.list_extend(sources, null_ls.builtins.formatting.sql_formatter) end
+
         require("null-ls").setup({
-            sources = {
-                null_ls.builtins.formatting.yamlfmt,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.sql_formatter,
-            },
+            sources = sources,
         })
     end
 }
