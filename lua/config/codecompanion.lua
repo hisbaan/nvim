62628b36b6803b60cffa61c52501542c7d7e8b01
hisbaan <hisbaan@gmail.com>
2025-12-09 11:31:39 -0500

    feat(codecompanion): switch to gemini-3-pro

--- lua/config/codecompanion.lua
+++ lua/config/codecompanion.lua
@@ -4,30 +4,29 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	config = function()
-		require("codecompanion").setup({
-			opts = {
-				log_level = "DEBUG",
-			},
-			strategies = {
-				chat = {
-					adapter = {
-						name = "gemini",
-						model = "gemini-2.5-pro",
-						-- model = "gemini-3-pro-preview",
-					},
-				},
-				inline = {
-					adapter = "copilot",
-				},
-				cmd = {
-					adapter = {
-						name = "gemini",
-						model = "gemini-2.5-pro",
-						-- model = "gemini-3-pro-preview",
-					},
+	opts = {
+		opts = {
+			log_level = "DEBUG",
+		},
+		strategies = {
+			chat = {
+				adapter = {
+					name = "gemini",
+					model = "gemini-3-pro-preview",
 				},
 			},
-		})
-	end,
+			inline = {
+				adapter = {
+					name = "gemini",
+					model = "gemini-3-pro-preview",
+				},
+			},
+			cmd = {
+				adapter = {
+					name = "gemini",
+					model = "gemini-3-pro-preview",
+				},
+			},
+		},
+	},
 }
