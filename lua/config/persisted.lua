0f656a88099122623ba6c2c33a0a1115e7ae1c2e
hisbaan <hisbaan@gmail.com>
2026-02-05 15:58:59 -0500

    feat(persisted): update keybind configs with valid commands

--- lua/config/persisted.lua
+++ lua/config/persisted.lua
@@ -1,31 +1,31 @@
-local opts = require('opts');
+local opts = require("opts")
 
 return {
-  "olimorris/persisted.nvim",
-  event = "BufReadPre",
-  lazy = false,
-  keys = {
-    { '<leader>ss', '<Cmd>SessionSelect<CR>',  opts, desc = "Search Sessions" },
-    { '<leader>sd', '<Cmd>SessionDelete<CR>',  opts, desc = "Delete Session" },
-    { '<leader>sr', '<Cmd>SessionRestore<CR>', opts, desc = "Restore Session" },
-    { '<leader>sl', '<Cmd>SessionLoad<CR>',    opts, desc = "Restore Session" },
-    { '<leader>sS', '<Cmd>SessionSave<CR>',    opts, desc = "Save Session" },
-  },
-  opts = function()
-    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
+	"olimorris/persisted.nvim",
+	event = "BufReadPre",
+	lazy = false,
+	keys = {
+		{ "<leader>sS", "<Cmd>Persisted select<CR>", opts, desc = "Select Session" },
+		{ "<leader>sd", "<Cmd>Persisted delete_current<CR>", opts, desc = "Delete Current Session" },
+		{ "<leader>sD", "<Cmd>Persisted delete<CR>", opts, desc = "Delete Session" },
+		{ "<leader>sl", "<Cmd>Persisted load<CR>", opts, desc = "Restore Session" },
+		{ "<leader>ss", "<Cmd>Persisted save<CR>", opts, desc = "Save Session" },
+	},
+	opts = function()
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 
-    return {
-      autostart = true,
-      autoload = true,
-      follow_cwd = true,
-      use_git_branch = true,
-      ignored_dirs = {
-        { "/",           exact = true },
-        { "~/",          exact = true },
-        { "~/Downloads", exact = true },
-        "~/.config/nvim",
-        "~/Documents/obsidian",
-      }
-    }
-  end,
+		return {
+			autostart = true,
+			autoload = true,
+			follow_cwd = true,
+			use_git_branch = true,
+			ignored_dirs = {
+				{ "/", exact = true },
+				{ "~/", exact = true },
+				{ "~/Downloads", exact = true },
+				"~/.config/nvim",
+				"~/Documents/obsidian",
+			},
+		}
+	end,
 }
